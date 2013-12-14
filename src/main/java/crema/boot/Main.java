1ebc902e0056b399427bf5f297b8040156fd4a22
Jason Andersen <andersen.jason@gmail.com>
2013-12-14 13:19:36 -0800

    booting up the Spring application context and registering a shutdown hook

--- src/main/java/crema/boot/Main.java
+++ src/main/java/crema/boot/Main.java
@@ -1,6 +1,11 @@
 package crema.boot;
 
 import org.apache.pivot.wtk.DesktopApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import crema.view.pivot.CremaPivotApplication;
 
@@ -11,12 +16,28 @@ import crema.view.pivot.CremaPivotApplication;
  */
 public class Main {
 
+    private static Logger log = LoggerFactory.getLogger(Main.class);
+
+    private static final String APP_CONTEXT_PATH = "spring/applicationContext.xml";
+
     /**
      * Boot strap method. Let's get the party started!
      * @param args
      */
     public static void main(String[] args) {
+        log.info("application booting up in desktop application mode");
+        initializeApplicationContext();
         DesktopApplicationContext.main(CremaPivotApplication.class, args);
+    }
+
+    /**
+     * initialize Spring's application context
+     */
+    private static ApplicationContext initializeApplicationContext() {
+        log.info("initializing Spring application context from {}", APP_CONTEXT_PATH);
+        ApplicationContext context = new ClassPathXmlApplicationContext(APP_CONTEXT_PATH);
+        ((ConfigurableApplicationContext) context).registerShutdownHook();
+        return context;
     }
 
 }
