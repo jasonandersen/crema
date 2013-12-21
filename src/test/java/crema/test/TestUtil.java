191b99ff0cd6fa98cde9837c3c95d9292a15c3a8
Jason Andersen <andersen.jason@gmail.com>
2013-12-21 10:00:13 -0800

    adding some logging statements to track down problems with the Cloudbees jenkins build

--- src/test/java/crema/test/TestUtil.java
+++ src/test/java/crema/test/TestUtil.java
@@ -4,6 +4,9 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.File;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import crema.util.PathUtil;
 
 /**
@@ -13,16 +16,20 @@ import crema.util.PathUtil;
  */
 public class TestUtil {
 
+    private static Logger log = LoggerFactory.getLogger(TestUtil.class);
+
     /**
      * Sets up a directory on the file system to use as a media directory for testing
      * @return a file object representing an existing directory to use for testing
      */
     public static File buildTestMediaDirectory(Class<?> callingClass) {
         String path = PathUtil.getJavaTempDirectoryPath();
-        String directoryName = String.format("%s-%d", callingClass.getClass().getSimpleName(),
+        String directoryName = String.format("%s-%d", callingClass.getSimpleName(),
                 System.currentTimeMillis());
         File directory = new File(String.format("%s%s", path, directoryName));
-        directory.mkdirs();
+        log.debug("creating test media directory: {}", directory);
+        boolean result = directory.mkdirs();
+        log.debug("test media direction creation result: {}", result);
         assertTrue(directory.exists());
         assertTrue(directory.canRead());
         assertTrue(directory.canWrite());
