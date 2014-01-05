32932a795864ac61750f53b4d67c1f9244acec34
Jason Andersen <andersen.jason@gmail.com>
2014-01-05 00:33:56 -0500

    refactored CommonMovieCrapWords to use the new classes for release year, display resolution and recording source

--- src/main/java/crema/util/text/CommonMovieCrapWords.java
+++ src/main/java/crema/util/text/CommonMovieCrapWords.java
@@ -4,6 +4,10 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.regex.Pattern;
 
+import crema.domain.DisplayResolution;
+import crema.domain.RecordingSource;
+import crema.util.BeanContext;
+
 /**
  * List of crap words that often appear in movie file names that aren't
  * part of the title itself.
@@ -16,40 +20,21 @@ public class CommonMovieCrapWords {
      * List of commonly found crap words in movie file names.
      */
     private static final String[] CRAP_PATTERNS = new String[] {
-            "dvdrip",
-            "dvd",
-            "dvdscr",
-            "brayrip",
-            "brrip",
-            "bray",
-            "blueray",
-            "bluray",
-            "br",
-            "bdrip",
             "AC3",
             "AAC",
             "xvid",
             "\\d\\dfps", /* frames per second */
-            "(19|20)\\d\\d", /* year */
-            "\\((19|20)\\d\\d\\)", /* year */
-            "\\[(19|20)\\d\\d\\]", /* year */
-            "\\{(19|20)\\d\\d\\}", /* year */
             "x264",
             "sd",
             "hd",
-            "480i",
-            "576i",
-            "576p",
-            "480p",
-            "720p",
-            "1080i",
-            "1080p",
             "\\[.*\\]" /* anything surrounded in brackets is suspect */
     };
 
     //TODO refactor to use ReleaseYearMatcher, RecordingSource and DisplayResolution
 
     private static final Collection<Pattern> PATTERNS;
+
+    private static ReleaseYearMatcher releaseYearMatcher;
 
     /**
      * Compile all the regex patterns
@@ -60,6 +45,7 @@ public class CommonMovieCrapWords {
             String regex = String.format("^%s$", crapPattern);
             PATTERNS.add(Pattern.compile(regex, Pattern.CASE_INSENSITIVE));
         }
+        releaseYearMatcher = BeanContext.getBean(ReleaseYearMatcher.class);
     }
 
     /**
@@ -75,6 +61,60 @@ public class CommonMovieCrapWords {
      * @return true if the argument matches a crap word
      */
     public static boolean isCrapWord(final String token) {
+        if (isReleaseYear(token)) {
+            return true;
+        }
+        if (isRecordingSource(token)) {
+            return true;
+        }
+        if (isDisplayResolution(token)) {
+            return true;
+        }
+        if (isMiscCrapWord(token)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param token
+     * @return
+     */
+    private static boolean isReleaseYear(final String token) {
+        return releaseYearMatcher.matches(token) != null;
+    }
+
+    /**
+     * @param token
+     * @return true if this token indicates a recording source
+     */
+    private static boolean isRecordingSource(final String token) {
+        for (RecordingSource source : RecordingSource.values()) {
+            if (source.matches(token)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param token
+     * @return true if this token indicates a display resolution
+     */
+    private static boolean isDisplayResolution(final String token) {
+        for (DisplayResolution resolution : DisplayResolution.values()) {
+            if (resolution.matches(token)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param token
+     * @return true if this token is one of the miscellaneous crap words defined earlier
+     */
+    private static boolean isMiscCrapWord(final String token) {
         for (Pattern pattern : PATTERNS) {
             if (pattern.matcher(token).matches()) {
                 return true;
